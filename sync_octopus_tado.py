f4fc3c44b11f59bcfd500290a502294114c7abae
Leon Armston <12968962+LeonArmston@users.noreply.github.com>
2024-10-22 17:49:09 +0100

    Update sync_octopus_tado.py

--- sync_octopus_tado.py
+++ sync_octopus_tado.py
@@ -76,8 +76,8 @@ def send_tarrif_to_tado(username, password, tarrif):
     
     
     
-    result = tado.set_eiq_tariff(from_date=valid_from, to_date=valid_to, tariff=value, is_period=True, unit="kWh")
-    print(f"Result send_reading_to_tado is {result}")
+    result = tado.set_eiq_tariff(from_date=valid_from, to_date=None, tariff=value, is_period=True, unit="kWh")
+    print(f"Result send_tarrif_to_tado is {result}")
 
 
 def parse_args():
